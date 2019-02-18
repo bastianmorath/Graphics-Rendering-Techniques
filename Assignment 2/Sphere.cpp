b5309b121c0f30758287ee9ad68ed36a65daf1b6
bastianmorath <bastian.morath@gmx.ch>
2019-02-18 16:10:22 +0800

    Added Assignment 2

---
+++ Assignment 2/Sphere.cpp
@@ -0,0 +1,41 @@
+//============================================================
+// STUDENT NAME: <your name>
+// MATRIC NO.  : <matric no.>
+// NUS EMAIL   : <your NUS email address>
+// COMMENTS TO GRADER:
+// <comments to grader, if any>
+//
+// ============================================================
+//
+// FILE: Sphere.cpp
+
+
+
+#include <cmath>
+#include "Sphere.h"
+
+using namespace std;
+
+
+
+bool Sphere::hit( const Ray &r, double tmin, double tmax, SurfaceHitRecord &rec ) const 
+{
+	//***********************************************
+    //*********** WRITE YOUR CODE HERE **************
+    //***********************************************
+
+	return false; // You can remove/change this if needed.
+}
+
+
+
+
+bool Sphere::shadowHit( const Ray &r, double tmin, double tmax ) const 
+{
+	//***********************************************
+    //*********** WRITE YOUR CODE HERE **************
+    //***********************************************
+
+	return false; // You can remove/change this if needed.
+}
+
